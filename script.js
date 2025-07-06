8c65d3ab88d263fcdcd41ab0d82ec21694cd502b
Md. Maksuduzzaman Maun <heemaun@gmail.com>
2025-07-06 11:42:17 +0600

    done for small devices except gallery

--- script.js
+++ script.js
@@ -20,3 +20,46 @@ $("#header_toggler").click(function(){
     $("nav").toggleClass("nav-hide");
     $(this).toggleClass("header-toggler-hide");
 });
+
+function isInViewport(element) {
+    const rect = element[0].getBoundingClientRect();
+
+    return (
+        rect.top < window.innerHeight && 
+        rect.bottom > 0
+    );
+}
+
+function checkVisibility() {
+    $('#skills').each(function () {
+        const $el = $(this);
+
+        if (isInViewport($el)) {
+            $el.addClass('skills-visible');
+        }
+
+        else{
+            $el.removeClass('skills-visible');
+        }
+    });
+}
+
+$(document).ready(function () {
+    checkVisibility();
+    $(window).on('scroll resize', checkVisibility);
+});
+
+$("#gallery button,.gallery-close").click(function(){
+    $("#"+$(this).attr("data-target")).toggleClass("gallery-details-hide");
+});
+
+$("#contacts .infos a").click(function(e){
+    // e.preventDefault();
+    // console.log($(this).attr("data-content"));
+    
+    const text = $(this).attr("data-content");
+    
+    navigator.clipboard.writeText(text)
+      .then(() => alert("Copied!"))
+      .catch(err => console.error("Failed to copy:", err));
+});
